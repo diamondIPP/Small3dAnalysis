9f1040433fb9939e22d433048afd1bc3da566b3a
diegoasanz <diegoasanz@gmail.com>
2018-05-04 19:53:11 +0200

    added logs directory to store the logfiles

--- Classes/SmallRD42Analysis.py
+++ Classes/SmallRD42Analysis.py
@@ -21,7 +21,7 @@ class SmallRD42Analysis:
         self.runWatch = ro.TStopwatch()
         self.runWatch.Start(True)
         time = ro.TDatime()
-        logging.basicConfig(filename='analysis_log_{r}_{y}_{m}_{d}_{h}.{min}.{sec}.log'.format(r=self.settings.run, y=time.GetYear(), m=time.GetMonth(), d=time.GetDay(), h=time.GetHour(), min=time.GetMinute(), sec=time.GetSecond()), level=logging.DEBUG)
+        logging.basicConfig(filename='{ap}/logs/analysis_log_{r}_{y}_{m}_{d}_{h}.{min}.{sec}.log'.format(ap=self.settings.analysis_path, r=self.settings.run, y=time.GetYear(), m=time.GetMonth(), d=time.GetDay(), h=time.GetHour(), min=time.GetMinute(), sec=time.GetSecond()), level=logging.DEBUG)
         CreateDirectoryIfNecessary('{d}/{s}/{r}'.format(d=self.settings.output_dir, s=self.settings.sub_dir, r=self.settings.run))
         self.converter = None
 
